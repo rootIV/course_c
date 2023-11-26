cdcec988adf18104eee157650ed88eddeae3d1b9
vitorwave <vitordevcontato@gmail.com>
2023-11-26 00:48:40 -0300

    Progress: Class 11 of 23

---
+++ class10/loops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void)
+{
+ // unsigned int i;
+
+//loop:
+  for(unsigned int i = 0; i < 3; i++)
+  {
+    printf("O valor de i é: %u\n", i);
+  }
+
+  //i++;
+
+  /*if(i < 3)
+   goto loop;*/
+
+  return 0;
+}
